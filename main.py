88047224313a508854ac1ab041984baf66b43bc5
ov8525 <72822874+ov8525@users.noreply.github.com>
2021-01-12 10:44:10 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+while True:
+    if input.sound_level() > 50:
+        music.siren.play()  
+        pause(5000)
+        music.stop_all_sounds() 
+    if input.light_level() < 20: 
+        light.show_animation(light.rainbowAnimation, 5000)
+    else: 
+        light.set_all(rgb.(0,0,0)
+    
--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+while (true) {
+    if (input.soundLevel() > 50) {
+        music.siren.play()
+        pause(5000)
+        music.stopAllSounds()
+    }
+    
+    if (input.lightLevel() < 20) {
+        light.showAnimation(light.rainbowAnimation, 5000)
+    } else {
+        light.setAll
+    }
+    
+}
